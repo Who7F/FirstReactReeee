894bc9fca89c839c496ed655c67d777e7c2dc7a8
Who7F <34862598+Who7F@users.noreply.github.com>
2023-05-19 12:37:24 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './Whatmean.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Lets go. Reeee react time
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Who7F"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github  
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
